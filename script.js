7164bf33e884c45adf25214873b8a7f047a0b3d4
Iamdodo22 <asimiazeez493@gail.com>
2023-11-18 10:57:47 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+"use strict";
+
+// variables
+const resultDisplay = document.querySelector('.displayResult');
+const keyDisplay = document.querySelector('.displayKeyValue');
+
+const keys = Array.from(document.querySelectorAll('.grid div'));
+const themeSwitchers = Array.from(document.querySelectorAll('.themeCon li'));
+const themeColor = document.querySelector('.themeColor')
+
+let theme = 0;
+let transX = 0;
+
+// functions
+function themeSetting(theme, transX) {
+    document.documentElement.style.setProperty('--key-color',`var(--key-color${theme})`); 
+    document.documentElement.style.setProperty('--key-colorShadow',`var(--key-colorShadow${theme})`);
+    document.documentElement.style.setProperty('--key-DR',`var(--key-DR${theme})`);
+    document.documentElement.style.setProperty('--key-DR-shadow',`var(--key-DR-shadow${theme})`);
+    document.documentElement.style.setProperty('--key-last',`var(--key-last${theme})`);
+    document.documentElement.style.setProperty('--key-last-shadow',`var(--key-last-shadow${theme})`);
+    document.documentElement.style.setProperty('--bg-mainBg',`var(--bg-mainBg${theme})`);
+    document.documentElement.style.setProperty('--textColor',`var(--textColor${theme})`);
+    document.documentElement.style.setProperty('--bg-keyBgToggle',`var(--bg-keyBgToggle${theme})`);
+    document.documentElement.style.setProperty('--key-textColor',`var(--key-textColor${theme})`);
+    document.documentElement.style.setProperty('--lastkey-color',`var(--lastkey-color${theme})`);
+   
+    themeColor.style.translate = `${transX}rem`; 
+}
+
+function init() {
+    resultDisplay.textContent='';
+    keyDisplay.textContent='';
+
+    let myTheme = JSON.parse(localStorage.getItem('theme')) || 0;
+    let transX =  JSON.parse(localStorage.getItem('transX')) || 0;
+
+    themeSetting(myTheme,transX)
+}
+
+function displayKeyValue() {
+    if(this.dataset.key === 'delete')return keyDisplay.textContent = keyDisplay.textContent.slice(0,-1);
+    
+    if(this.dataset.key === '=')return resultDisplay.textContent = eval(keyDisplay.textContent);
+    keyDisplay.textContent += this.dataset.key;
+    if(this.dataset.key === 'reset'){
+        keyDisplay.textContent ='';
+        resultDisplay.textContent ='';
+    }
+
+}
+
+function switchTheme(e) {
+    if(e.target === themeSwitchers[0]){
+      theme = 0; 
+    transX = 0;
+    } 
+    if(e.target === themeSwitchers[1]){
+       theme = 1;
+       transX = 1.25; 
+    } 
+    if(e.target === themeSwitchers[2]){
+      theme = 2;
+      transX = 2.25; 
+    } 
+
+    localStorage.setItem('theme', JSON.stringify(theme));
+    localStorage.setItem('transX', JSON.stringify(transX));
+    themeSetting(theme, transX)
+}
+
+// event handler
+window.addEventListener('DOMContentLoaded',init )
+
+keys.forEach(function(key) {
+    key.addEventListener('click', displayKeyValue)
+})
+
+themeSwitchers.forEach(function(theme) {
+    theme.addEventListener('click', switchTheme)
+})
---
+++ tailwind.config.js
@@ -0,0 +1,53 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./script.js"],
+  theme: {
+    extend: {
+      colors:{
+        mainBg0: "hsl(222, 26%, 31%)", 
+        keyPadBg0: "hsl(223, 31%, 20%)", 
+        screenBg0: "hsl(224, 36%, 15%)" ,
+        keyBg0: "hsl(225, 21%, 49%)",
+        keyShadow1 : "hsl(224, 28%, 35%)",
+        keyBgToggle0: "hsl(6, 63%, 50%)",
+        keyshadow00: "hsl(6, 70%, 34%)",
+        keyBg00: "hsl(30, 25%, 89%)",
+        keyShadow000: "hsl(28, 16%, 65%)",
+        text0: "hsl(221, 14%, 31%)",
+        pWhite: "hsl(0, 0%, 100%)",
+
+      // theme2
+      mainBg1: "hsl(0, 0%, 90%)", 
+      keyPadBg1: "hsl(0, 5%, 81%)", 
+      screenBg1: " hsl(0, 0%, 93%)" ,
+      keyBg1: "hsl(185, 42%, 37%)",
+      keyShadow1: "hsl(185, 58%, 25%)",
+      keyBgToggle1: "hsl(25, 98%, 40%)",
+      keyshadow11: "hsl(25, 99%, 27%)",
+      keyBg11: "hsl(45, 7%, 89%)",
+      keyShadow111: "hsl(35, 11%, 61%)",
+      text1: "hsl(60, 10%, 19%)",
+
+      // theme3
+      mainBg2: " hsl(268, 75%, 9%)", 
+      keyPadBg2: "hsl(268, 71%, 12%)", 
+      screenBg2: " hsl(268, 71%, 12%)" ,
+      keyBg2: "hsl(281, 89%, 26%)",
+      keyShadow2: "hsl(285, 91%, 52%)",
+      keyBgToggle2: "hsl(176, 100%, 44%)",
+      keyshadow22: "hsl(177, 92%, 70%)",
+      keyBg22: " hsl(268, 47%, 21%)",
+      keyShadow222: "hsl(290, 70%, 36%)",
+      text2: "hsl(52, 100%, 62%)",
+      },
+      fontFamily:{
+        spartan: ['League Spartan']
+      },
+      fontSize:{
+        pText: ['32px']
+      }
+    },
+  },
+  plugins: [],
+}
+
